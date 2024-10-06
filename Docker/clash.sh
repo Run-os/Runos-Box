6283c394222de8d05a936b41e23e07d26524cd3e
Runos <77967997+Run-os@users.noreply.github.com>
2024-10-07 01:47:59 +0800

    1

--- Docker/clash.sh
+++ Docker/clash.sh
@@ -4,37 +4,63 @@
 docker_data="/vol1/1000/Docker"
 container="clash"
 
-sudo mkdir -p $docker_data/$container
+sudo mkdir -p $docker_data/$container/svc-clash
 
-# 配置文件
-sudo cat <<EOF > $docker_data/$container/config.yaml
+# test
+config=$(
 # config.yaml
 port: 7890
 socks-port: 7891
-allow-lan: true
+allow-lan: false
+mode: Rule
+log-level: silent
 external-controller: 0.0.0.0:9090
+secret: "123456"
+)
+
+compose=$(
+# docker-compose.yml
+version: "3"
+
+services:
+  # Clash
+  svc-clash:
+    image: dreamacro/clash
+    container_name: svc-clash
+    volumes:
+      - ./svc-clash/config.yaml:/root/.config/clash/config.yaml
+    ports:
+      - "7890:7890/tcp"
+      - "7890:7890/udp"
+      - "9090:9090"
+    restart: always
+    networks:
+      - default
+  # Clash Dashboard
+  svc-clash-dashboard:
+    image: centralx/clash-dashboard
+    container_name: svc-clash-dashboard
+    ports:
+      - "12345:80"
+    restart: always
+    networks:
+      - default
+
+# Networks
+networks:
+  default:
+    driver: bridge
+    name: svc
+)
+
+# 配置文件
+sudo cat <<EOF > $docker_data/$container/svc-clash/config.yaml
+$config
 EOF
 
 # docker-compose
 sudo cat <<EOF > $docker_data/$container/docker-compose.yml
-# docker-compose.yml
-version: '3.7'
-services:
-	clash-server:
-	    image: dreamacro/clash
-	    container_name: clash
-	    ports:
-	      - "5090:9090"
-	      - "5890:7890"
-	      - "5891:7891"
-	    volumes:
-	      - ./config.yaml:/root/.config/clash/config.yaml
-
-	clash-ui:
-	    image: haishanh/yacd
-	    container_name: clash-ui
-	    ports:
-	      - 5080:80
+$compose
 EOF
 
 cd $docker_data/$container
