2e1626315a2287ac8242608de2ea504a4863f6da
Runos <77967997+Run-os@users.noreply.github.com>
2024-10-07 02:53:15 +0800

    添加clouddrive

--- Docker/clash.sh
+++ Docker/clash.sh
@@ -4,20 +4,7 @@
 docker_data="/vol1/1000/Docker"
 container="clash"
 
-sudo mkdir -p $docker_data/$container/svc-clash
-
-# test
-config=$(cat <<'EOF'
-# config.yaml
-port: 7890
-socks-port: 7891
-allow-lan: false
-mode: Rule
-log-level: silent
-external-controller: 0.0.0.0:9090
-secret: "123456"
-EOF
-)
+sudo mkdir -p $docker_data/$container
 
 compose=$(cat <<'EOF'
 # docker-compose.yml
@@ -29,11 +16,10 @@ services:
     image: laoyutang/clash-and-dashboard:latest
     container_name: clash
     volumes:
-      - ./svc-clash/config.yaml:/root/.config/clash/config.yaml
+      - ./:/root/.config/clash
     ports:
-      - "7890:7890/tcp"
-      - "7890:7890/udp"
-      - "9090:9090"
+      - 7890:7890
+      - 7888:8080
     restart: always
     networks:
       - default
@@ -42,14 +28,9 @@ services:
 networks:
   default:
     driver: bridge
-    name: svc
+    name: clash
 EOF
 )
-
-# 配置文件
-sudo cat <<EOF > $docker_data/$container/svc-clash/config.yaml
-$config
-EOF
 
 # docker-compose
 sudo cat <<EOF > $docker_data/$container/docker-compose.yml
---
+++ Docker/clouddrive.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# 变量
+docker_data="/vol1/1000/Docker"
+container="Clouddrive"
+
+sudo mkdir -p /etc/systemd/system/docker.service.d/
+sudo cat <<EOF > /etc/systemd/system/docker.service.d/clear_mount_propagation_flags.conf
+[Service]
+MountFlags=shared
+EOF
+sudo systemctl restart docker.service
+
+compose=$(cat <<'EOF'
+# docker-compose.yml
+version: "2.1"
+services:
+  cloudnas:
+    image: cloudnas/clouddrive2
+    container_name: clouddrive2
+    environment:
+      - TZ=Asia/Shanghai
+      - CLOUDDRIVE_HOME=/Config
+    volumes:
+      - /vol1/1000/Clouddrive/shared:/CloudNAS:shared
+      - /vol1/1000/Clouddrive/Config:/Config
+      - /vol1/1000/Clouddrive/media/shared:/media:shared #optional media path of host
+    devices:
+      - /dev/fuse:/dev/fuse
+    restart: unless-stopped
+    pid: "host"
+    privileged: true
+    network_mode: "host"
+EOF
+)
+
+# docker-compose
+sudo cat <<EOF > $docker_data/$container/docker-compose.yml
+$compose
+EOF
+
+cd $docker_data/$container
+sudo docker-compose up -d
