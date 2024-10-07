4165a0d4a9f2aaacde5c4d7a837b83f4fc97650a
Runos <77967997+Run-os@users.noreply.github.com>
2024-10-07 15:34:31 +0800

    1

--- Docker/navidrome.sh
+++ Docker/navidrome.sh
@@ -11,27 +11,20 @@ version: "3"
 services:
   navidrome:
     container_name: navidrome
-    image: deluan/navidrome:develop
+    image: deluan/navidrome:latest
     ports:
       - "4533:4533"
     restart: always
     environment:
-      ND_SCANSCHEDULE: 0
-      ND_LOGLEVEL: info
+      ND_ENABLETRANSCODINGCONFIG: true
+      ND_TRANSCODINGCACHESIZE: 0
+      ND_SCANSCHEDULE: 1h
+      ND_LOGLEVEL: info  
       ND_SESSIONTIMEOUT: 24h
-      ND_BASEURL: "/nav"
-      ND_PLAYLISTSPATH: "."
-      ND_LASTFM_LANGUAGE: "zh"
-      ND_LASTFM_APIKEY: "lastfm_apikey"
-      ND_LASTFM_SECRET: "lastfm_secret"
-      ND_SPOTIFY_ID: "spotify_id"
-      ND_SPOTIFY_SECRET: "spotify_secret"
-      ND_ENABLEARTWORKPRECACHE: "false"
-      ND_ENABLESHARING: "true"
+      ND_BASEURL: ""
     volumes:
       - "/vol1/1000/Docker/navidrome/data:/data"
       - "/vol1/1000/Music:/music:ro"
-
 EOF
 )
 
